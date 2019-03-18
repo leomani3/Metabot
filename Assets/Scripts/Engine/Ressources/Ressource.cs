f99e9377a0c6d2aa2ef00b92ca117ce88eb235a4
TheWincher <emmanuel.louchez@etu.univ-grenoble-alpes.fr>
2019-03-18 20:51:33 +0100

    création de la classe unit et ressource

---
+++ Assets/Scripts/Engine/Ressources/Ressource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ressource
+{
+
+    string name;
+    int weight;
+    float respawnTime;
+
+    public Ressource(string name, int weight, float respawnTime)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.respawnTime = respawnTime;
+    }
+
+}
+
