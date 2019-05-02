3e9fb02950956106db5c6eaffbdf45f567ec2a80
Aloïs <alois.marrocq@etu.umontpellier.fr>
2019-05-02 18:09:15 +0200

    ok

--- Assets/Scripts/Engine/Unit/Action/Condition.cs
+++ Assets/Scripts/Engine/Unit/Action/Condition.cs
@@ -12,10 +12,9 @@ public class Condition
 	public Condition(string text){
 		stringExpression = text;
 	}
-	
-<<<<<<< HEAD
-	public bool satisfied(Unit unit){	
-		
+    
+    public bool satisfied(Unit unit) {
+
         switch (operateur)
         {
             case ">":
@@ -30,26 +29,5 @@ public class Condition
                 return unit.LookUp(param1) <= unit.LookUp(param2);
         }
         return false;
-=======
-	public bool satisfied(Unit unit){
-        /*//remplacer les variables ecrites en string par leur valeur
-		stringExpression.Replace("currenHealth", unit.CurrentHealth);
-		//--
-		
-		return evaluate(stringExpression);*/
-        return true;
-	}
-	
-	public bool evaluate(string logicalExpression){
-        /*System.Data.DataTable table = new System.Data.DataTable();
-		table.Columns.Add("", typeof(bool));
-		table.Columns[0].Expression = logicalExpression;
-
-		System.Data.DataRow r = table.NewRow();
-		table.Rows.Add(r);
-		bool result = (Boolean)r[0];
-		return result;*/
-        return true;
->>>>>>> 2dfc69d3fd25afbea915b43422c2cd9a4fba0fed
-	}
+    }
 }
