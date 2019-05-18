d19b7d480233295e6a3782f6853c14bad705e4ac
Aloïs <alois.marrocq@etu.umontpellier.fr>
2019-05-18 15:35:24 +0200

    Alois

--- Assets/Scripts/Engine/Unit/WarUnit/MovableUnit.cs
+++ Assets/Scripts/Engine/Unit/WarUnit/MovableUnit.cs
@@ -9,9 +9,8 @@ public abstract class MovableUnit : WarUnit
 
     public MovableUnit(MetaTeam team, 
         float maxHealth = 200, float speed = 1.8f, float distanceSight = 20.0f, 
-        float angleSight = 180.0f, int maxBagSize = 5, float heading = 45.0f, 
-        float timeReload = 1.0f, float armor = 1.0f) 
-        : base(team, maxHealth, distanceSight, angleSight, maxBagSize, heading, timeReload, armor)
+        float angleSight = 180.0f, int maxBagSize = 5, float heading = 45.0f, float armor = 1.0f) 
+        : base(team, maxHealth, distanceSight, angleSight, maxBagSize, heading, armor)
     {
         this.movableFeature = new Movable(this, speed);    //permet à l'unité de bouger
     }
