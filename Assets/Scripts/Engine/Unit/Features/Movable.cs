aaa3f9ab0453a3088eac6042f565ef22ab36d570
Aloïs <alois.marrocq@etu.umontpellier.fr>
2019-05-28 19:19:04 +0200

    Alois

--- Assets/Scripts/Engine/Unit/Features/Movable.cs
+++ Assets/Scripts/Engine/Unit/Features/Movable.cs
@@ -13,6 +13,7 @@ public class Movable : Feature
 
     public void Move()
     {
+        unit.Heading = (unit.Heading + Random.Range(-5, 5)) % 360;
         unit.Unit_go.transform.position += speed * Utility.vectorFromAngle(unit.Heading).normalized * 0.1f; //*0.2f normalement
     }
 
